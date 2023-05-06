b388825522525582ef559997202134feba5571f7
David Nowinsky <david@nowinsky.net>
2023-05-06 13:47:47 +0200

    Add second relay with blinking state

--- arduino/analog0_to_serial.ino
+++ arduino/analog0_to_serial.ino
@@ -9,3 +9,76 @@ void loop() {
   Serial.println(analogValue); // Send the value through the serial port
   delay(100); // Wait 100 milliseconds before reading the value again
 }
+const int analogPin = A0; // Choose the analog pin you want to read
+
+const int digitalPinRelayTV = 12;
+const char ON_RELAY_TV_VALUE = '1';
+const char OFF_RELAY_TV_VALUE = '0';
+bool relayTVState = false;
+
+const int digitalPinRelayLights = 8;
+const char ON_RELAY_LIGHTS_VALUE = '3';
+const char OFF_RELAY_LIGHTS_VALUE = '4';
+const char BLINK_RELAY_LIGHTS_VALUE = '5';
+bool relayLightsState = false;
+bool blinkRelayLightsCounter = 2;
+bool blinkingRelayLightsState = false;
+
+void setup()
+{
+  Serial.begin(9600); // Set the baud rate for serial communication
+  pinMode(digitalPinRelayTV, OUTPUT);
+  pinMode(digitalPinRelayLights, OUTPUT);
+}
+
+void loop()
+{
+  int analogValue = analogRead(analogPin); // Read the value from the analog pin
+  Serial.println(analogValue);             // Send the value through the serial port
+
+  // Check if there is data available on the serial port
+  while (Serial.available() > 0)
+  {
+    char command = Serial.read(); // Read the command sent by Python
+
+    if (command == ON_RELAY_TV_VALUE)
+    {
+      relayTVState = true;
+    }
+    else if (command == OFF_RELAY_TV_VALUE)
+    {
+      relayTVState = false;
+    }
+    else if (command == ON_RELAY_LIGHTS_VALUE)
+    {
+      relayLightsState = true;
+      blinkingRelayLightsState = false;
+    }
+    else if (command == OFF_RELAY_LIGHTS_VALUE)
+    {
+      relayLightsState = false;
+      blinkingRelayLightsState = false;
+    }
+    else if (command == BLINK_RELAY_LIGHTS_VALUE)
+    {
+      relayLightsState != relayLightsState;
+      blinkingRelayLightsState = true;
+      blinkRelayLightsCounter = 2;
+    }
+  }
+
+  if (blinkingRelayLightsState)
+  {
+    blinkRelayLightsCounter = blinkRelayLightsCounter - 1;
+    if (blinkRelayLightsCounter <= 0)
+    {
+      relayLightsState = !relayLightsState;
+      blinkRelayLightsCounter = 2;
+    }
+  }
+
+  digitalWrite(digitalPinRelayLights, relayLightsState ? HIGH : LOW);
+  digitalWrite(digitalPinRelayTV, relayTVState ? HIGH : LOW);
+
+  delay(100); // Wait 100 milliseconds before reading the value again
+}
--- src/main.py
+++ src/main.py
@@ -1,46 +1,81 @@
+from typing import Literal
 import serial
 import time
 
+ARDUINO_SERIAl_PORT = '/dev/ttyACM0'
+
+ON_RELAY_TV_VALUE = b'1'
+OFF_RELAY_TV_VALUE = b'0'
+
+ON_RELAY_LIGHTS_VALUE = b'3'
+OFF_RELAY_LIGHTS_VALUE = b'4'
+BLINK_RELAY_LIGHTS_VALUE = b'5'
 
 
-THRESHOLD_VALUE = 1
-GRACE_PERIOD_SECONDS = 1
+THRESHOLD_VALUE = 10
+GRACE_PERIOD_SECONDS = 2
 SERIAL_RATE_MS = 100
 
 
-current_state: bool = False
+current_tv_state: bool = False
 grace_period_counter: int = 0
+current_lights_state: bool | Literal['blink'] = False
 
 def on_value_change(val: int):
     global grace_period_counter
-    global current_state
+    global current_tv_state
+    global current_lights_state
 
-    previous_state = current_state
+    previous_tv_state = current_tv_state
+    previous_lights_state = current_lights_state
 
     if val > THRESHOLD_VALUE:
-        if not current_state:
-            current_state = True
+        if not current_tv_state:
+            current_tv_state = True
+            
+        if current_lights_state != True:
+            current_lights_state = True
             grace_period_counter = int(GRACE_PERIOD_SECONDS * 1000 / SERIAL_RATE_MS)
 
     else:
         if grace_period_counter <= 0:
-            current_state = False
+            current_tv_state = False
+            current_lights_state = False
         else:
             grace_period_counter = grace_period_counter - 1
+            current_lights_state = 'blink'
     
-    if previous_state != current_state:
-        on_state_change(current_state)
+    if previous_tv_state != current_tv_state:
+        on_state_tv_change(current_tv_state)
+    
+    if previous_lights_state != current_lights_state:
+        on_state_lights_change(current_lights_state)
 
 
-def on_state_change(state: bool):
-    if state:
-        print('Start')
+def on_state_tv_change(tv_state: bool):
+    if tv_state:
+        print('Start TV')
+        ser.write(ON_RELAY_TV_VALUE)
     else:
-        print('Stop')
+        print('Stop TV')
+        ser.write(OFF_RELAY_TV_VALUE)
+
+
+def on_state_lights_change(lights_state: bool | Literal['blink']):
+    match lights_state:
+        case True:
+            print('Start lights')
+            ser.write(ON_RELAY_LIGHTS_VALUE)
+        case 'blink':
+            print('Blink lights')
+            ser.write(BLINK_RELAY_LIGHTS_VALUE)
+        case False:
+            print('Stop lights')
+            ser.write(OFF_RELAY_LIGHTS_VALUE)
 
 
 # Configure the serial port and baud rate
-ser = serial.Serial('/dev/ttyACM0', 9600)  # Replace  with the appropriate port for your system
+ser = serial.Serial(ARDUINO_SERIAl_PORT, 9600)  # Replace  with the appropriate port for your system
 time.sleep(2)  # Wait for the serial connection to initialize
 
 while True:
